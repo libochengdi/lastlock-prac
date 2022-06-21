18519e30d3fc7587862e0a9957b80a4e6c7ba0e7
Eric Li <eric@lastlock.com>
2022-06-21 11:43:22 -0500

    First day of full-time internship q

--- fund_less5_exer1/src/main.c
+++ fund_less5_exer1/src/main.c
@@ -6,22 +6,16 @@ Controlling LEDs through UART. Press 1-4 on your keyboard to toggle LEDS 1-4 on 
 #include <devicetree.h>
 #include <drivers/gpio.h>
 #include <sys/printk.h>
-/* STEP 3 - Include the header file of the UART driver in main.c */
-#include <drivers/uart.h>
 
-#include <drivers/i2c.h>
+#include <drivers/uart.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
-/* STEP 9.1.1 - Define the size of the receive buffer */
-#define RECEIVE_BUFF_SIZE 10
+#define ERORR 1
+#define SUCCESS 0
 
-/* STEP 9.2 - Define the receiving timeout period */
-#define RECEIVE_TIMEOUT   100
-
-/* STEP 5.1 - Get C identifiers for the DeviceTree labels and properties' values related to LEDs */
-// Defines for controlling LED with UART
+/* LED pin numbers */
 #define LED0_NODE 	DT_ALIAS(led0)
 #define LED0		DT_GPIO_LABEL(LED0_NODE, gpios)
 #define PIN0		DT_GPIO_PIN(LED0_NODE, gpios)
@@ -35,51 +29,27 @@ Controlling LEDs through UART. Press 1-4 on your keyboard to toggle LEDS 1-4 on 
 #define PIN2		DT_GPIO_PIN(LED2_NODE, gpios)
 #define FLAG2		DT_GPIO_FLAGS(LED2_NODE, gpios)
 
-#define I2C0_NODE   DT_NODELABEL(i2c0)
+/* STEP 9.1.1 - Define the size of the receive buffer */
 
-/* The devicetree node identifier for the "i2c0" */
-#define I2C0_NODE DT_NODELABEL(i2c0)
-#if DT_NODE_HAS_STATUS(I2C0_NODE, okay)
-#define I2C0	DT_LABEL(I2C0_NODE)
-#else
-/* A build error here means your board does not have I2C enabled. */
-#error "i2c0 devicetree node is disabled"
-#define I2C0	""
-#endif
+/* STEP 9.2 - Define the receiving timeout period */
 
+/* STEP 5.1 - Get C identifiers for the DeviceTree labels and properties' values related to LEDs */
 
 static const struct device *leds;
 
 /* STEP 8.1 - Define the transmission buffer, which is a buffer to hold the data to be sent over UART */
-static uint8_t tx_buf[] = {"nRF Connect SDK Fundamentals Course \n\r"
-						   "Press 1-3 on your keyboard to toggle LEDS 1-3 on your developmnent kit \n\r"};
+
 
 /* STEP 9.1.2 - Define the receive buffer */
-static uint8_t rx_buf[RECEIVE_BUFF_SIZE] = {0};
+
 
 /* STEP 6 - Define the callback function for UART */
-static void uart_cb(const struct device *dev, struct uart_event *evt, void *user_data)
-{
-	switch (evt->type) {
 
-	case UART_RX_RDY:
-		if((evt->data.rx.len) == 1) {
-
-			if(evt->data.rx.buf[evt->data.rx.offset] == '1')
-				gpio_pin_toggle(leds,PIN0);
-			else if (evt->data.rx.buf[evt->data.rx.offset] == '2')
-				gpio_pin_toggle(leds,PIN1);
-			else if (evt->data.rx.buf[evt->data.rx.offset] == '3')
-				gpio_pin_toggle(leds,PIN2);					
-			break;
-		}
-		
-	case UART_RX_DISABLED:
-		uart_rx_enable(dev ,rx_buf,sizeof rx_buf,RECEIVE_TIMEOUT);
-		break;
-		
-	default:
-		break;
+void CheckError(int err) {
+	if (err != 0) {
+		exit(err);
+	} else {
+		; // Nowhere man in nowhere lands...
 	}
 }
 
@@ -91,46 +61,28 @@ int main(void)
 		return 1 ;
 	}
 
-	/* STEP 4 - Get the device struct of the UART hardware */
+/* STEP 4 - Get the device struct of the UART hardware */
 	const struct device *uart = device_get_binding(DT_LABEL(DT_NODELABEL(uart0)));
 	if (uart == NULL) {
-		printk("Could not find %s! \n\r", DT_LABEL(DT_NODELABEL(uart0)));
-		return 1;
+		printk("Cannot get UART device\n\r");
+		return ERROR;
 	}
 
-	/* STEP 5.2 - Configure the LEDs */
 	ret = gpio_pin_configure(leds, PIN0, GPIO_OUTPUT_ACTIVE | FLAG0);
-	if (ret < 0) {
-		return 1;
-	}
+	CheckError(ret);
 
 	ret = gpio_pin_configure(leds, PIN1, GPIO_OUTPUT_ACTIVE | FLAG1);
-	if (ret < 0) {
-		return 1;
-	}
+	CheckError(ret);
 
 	ret = gpio_pin_configure(leds, PIN2, GPIO_OUTPUT_ACTIVE | FLAG2);
-	if (ret < 0) {
-		return 1;
-	}
+	CheckError(ret);
+/* STEP 5.2 - Configure the LEDs */
 
-	/* STEP 7 - Register the UART callback function */
-	ret = uart_callback_set(uart, uart_cb, NULL);
-	if (ret) {
-		return 1;
-	}
+/* STEP 7 - Register the UART callback function */
 
-	/* STEP 8.2 - Send the data over UART by calling uart_tx() */
-	ret = uart_tx(uart, tx_buf, sizeof(tx_buf), SYS_FOREVER_MS);
-	if (ret) {
-		return 1;
-	}
+/* STEP 8.2 - Send the data over UART by calling uart_tx() */
 
-	/* STEP 9.3  - Start receiving by calling uart_rx_enable() and pass it the address of the receive  buffer */
-	ret = uart_rx_enable(uart, rx_buf, sizeof(rx_buf), RECEIVE_TIMEOUT);
-	if (ret) {
-		return 1;
-	}
+/* STEP 9.3  - Start receiving by calling uart_rx_enable() and pass it the address of the receive  buffer */
 
 	while (1) {
 		k_msleep(SLEEP_TIME_MS);
